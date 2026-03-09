c98a92c0b3f8d43a2643861abd76a7bf0571367e
Jackelyne <Jackelyneramosbrito@gmail.com>
2026-03-09 10:03:26 -0300

    bonus concluido✔😃

--- main.py
+++ main.py
@@ -26,3 +26,27 @@ def menu():
         print("7. Relatório: Disponíveis") # Bônus
         print("8. Relatório: Emprestados") # Bônus
         print("9. Sair")
+
+        op = input("Escolha uma opção: ")
+
+        if op == "1":
+            bib.adicionar_item(Livro(input("Cód: "), input("Título: "), int(input("Ano: ")), input("Autor: "), input("Páginas: ")))
+        elif op == "2":
+            bib.adicionar_item(Revista(input("Cód: "), input("Título: "), int(input("Ano: ")), input("Edição: "), input("Mês: ")))
+        elif op == "3":
+            bib.adicionar_item(DVD(input("Cód: "), input("Título: "), int(input("Ano: ")), input("Diretor: "), input("Duração(min): ")))
+        elif op == "4":
+            bib.listar_itens()
+        elif op == "5":
+            bib.emprestar_item(input("Código: "))
+        elif op == "6":
+            bib.devolver_item(input("Código: "))
+        elif op == "7":
+            bib.gerar_relatorio_status(disponivel=True)
+        elif op == "8":
+            bib.gerar_relatorio_status(disponivel=False)
+        elif op == "9":
+            break
+
+if __name__ == "__main__":
+    menu()
