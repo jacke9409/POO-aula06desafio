0ceb76e7c50e23dfa51f16ccd2ef94680e8cd5d1
Jackelyne <Jackelyneramosbrito@gmail.com>
2026-03-09 09:42:24 -0300

    main atualizada

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 #  Criar um sistema de gestão de itens de uma biblioteca, com cadastro, empréstimo e devolução
 # Regras do sistema( requisitos)
 
+def adicionar_item(self, novo_item):
+    # Validação de código único (Bônus)
+    if self.buscar_por_codigo(novo_item.codigo):
+        print(f"Erro: Código {novo_item.codigo} já existe!")
+    else:
+        self.__itens.append(novo_item)
